3e7cba4e1cb1ec662f6148dfc1d403bed81063ac
to77e <skzntcv@gmail.com>
2023-07-18 22:54:01 +0300

    feat: config

--- go.mod
+++ go.mod
@@ -9,8 +9,11 @@ require (
 
 require (
 	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394 // indirect
+	github.com/cristalhq/aconfig v0.18.4 // indirect
+	github.com/cristalhq/aconfig/aconfigyaml v0.17.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"github.com/cristalhq/aconfig/aconfigyaml"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/cristalhq/aconfig"
+)
+
+type Config struct {
+	TelegramBotToken     string        `yaml:"telegram_bot_token" env:"TELEGRAM_BOT_TOKEN" required:"true"`
+	TelegramChannelID    int64         `yaml:"telegram_channel_id" env:"TELEGRAM_CHANNEL_ID" required:"true"`
+	DatabaseDSN          string        `yaml:"database_dsn" env:"DATABASE_DSN" default:"postgres://postgres:postgres@localhost:5432/news?sslmode=disable"`
+	FetchInterval        time.Duration `yaml:"fetch_interval" env:"FETCH_INTERVAL" default:"10m"`
+	NotificationInterval time.Duration `yaml:"notification_interval" env:"NOTIFICATION_INTERVAL" default:"1h"`
+	FilterKeyword        []string      `yaml:"filter_keyword" env:"FILTER_KEYWORD"`
+	OpenAIKey            string        `yaml:"openai_key" env:"OPENAI_KEY"`
+	OpenAIPrompt         string        `yaml:"openai_prompt" env:"OPENAI_PROMPT"`
+}
+
+var (
+	cfg  *Config
+	once sync.Once
+)
+
+func Get() *Config {
+	once.Do(func() {
+		loader := aconfig.LoaderFor(&cfg, aconfig.Config{
+			EnvPrefix: "NFB",
+			Files:     []string{"./config.yaml", "./config.local.yaml"},
+			FileDecoders: map[string]aconfig.FileDecoder{
+				".yaml": aconfigyaml.New(),
+			},
+		})
+		if err := loader.Load(); err != nil {
+			log.Printf("[ERROR] load config: %v", err)
+		}
+	})
+	return cfg
+}
